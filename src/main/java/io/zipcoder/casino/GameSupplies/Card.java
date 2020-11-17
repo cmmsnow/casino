371effcdf32156eb7474f5c51dd8391cf70b2910
christian <christian@zipcodes-MBP-51.lan>
2020-11-17 11:43:19 -0500

    created a Card and DeckOfCards class

--- src/main/java/io/zipcoder/casino/GameSupplies/Card.java
+++ src/main/java/io/zipcoder/casino/GameSupplies/Card.java
@@ -1,4 +1,84 @@
 package io.zipcoder.casino.GameSupplies;
 
 public class Card {
+
+    private Suit suit;
+    private Rank value;
+
+    public Card(Suit suit, Rank value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public Suit getSuit() {
+        return this.suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+    public Rank getValue() {
+        return this.value;
+    }
+
+    public void setValue(Rank value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s of %s", this.value, this.suit); // can use this.value.getThirdValue() so that the string in the enum is returned
+    }
+
+}
+
+// ---- enums ----
+
+enum Suit {
+    SPADES, HEARTS, DIAMONDS, CLUBS
+}
+
+// Go Fish and Blackjack don't use the joker cards
+// created this ranking because ACE can have a value of 1 or 11 and JACK, QUEEN, and KING have the value of 10 also in GoFish you ask for "ace", "seven", etc
+enum Rank {
+
+    ACE(1, 11, "ace"),
+    TWO(2, 2, "two"),
+    THREE(3, 3, "three"),
+    FOUR(4, 4, "four"),
+    FIVE(5, 5, "five"),
+    SIX(6, 6, "six"),
+    SEVEN(7, 7, "seven"),
+    EIGHT(8, 8, "eight"),
+    NINE(9, 9, "nine"),
+    TEN(10, 10, "ten"),
+    JACK(11, 10, "jack"),
+    QUEEN(12, 10, "queen"),
+    KING(13, 10, "king");
+
+    private int primaryValue;
+
+    private int secondaryValue;
+
+    private String thirdValue;
+
+    Rank(int primaryValue, int secondaryValue, String thirdValue) {
+        this.primaryValue = primaryValue;
+        this.secondaryValue = secondaryValue;
+        this.thirdValue = thirdValue;
+    }
+
+    public int getPrimaryValue() {
+        return primaryValue;
+    }
+
+    public int getSecondaryValue() {
+        return secondaryValue;
+    }
+
+    public String getThirdValue() {
+        return thirdValue;
+    }
+
 }
--- src/main/java/io/zipcoder/casino/GameSupplies/DeckOfCards.java
+++ src/main/java/io/zipcoder/casino/GameSupplies/DeckOfCards.java
@@ -1,4 +1,53 @@
 package io.zipcoder.casino.GameSupplies;
 
+import java.util.Collections;
+import java.util.Stack;
+
 public class DeckOfCards {
+
+//    public static void main(String[] args) {
+//        DeckOfCards testDeck = new DeckOfCards();
+//        testDeck.refreshDeck();
+//        testDeck.shuffleDeck();
+//        int count = 0;
+//        for (Card c : testDeck.deckOfCards) {
+//            System.out.println(c);
+//            count++;
+//        }
+//        System.out.println("\n" + testDeck.dealCard().toString());
+//        System.out.print("\nnumber of cards: " + count);
+//    }
+
+    private Stack<Card> deckOfCards;
+
+    public DeckOfCards() {
+        refreshDeck();
+        this.deckOfCards = shuffleDeck();
+    }
+
+    public Stack<Card> getDeck() {
+        return deckOfCards;
+    }
+
+    private Stack<Card> shuffleDeck() {
+        Collections.shuffle(deckOfCards);
+        return deckOfCards;
+    }
+
+    public Card dealCard() {
+        return deckOfCards.pop();
+    }
+
+    private void refreshDeck() {
+        Stack<Card> newDeck = new Stack<Card>();
+        for (Rank r : Rank.values()) {
+            for (Suit s : Suit.values()) {
+                Card temp = new Card(s, r);
+                newDeck.push(temp);
+            }
+        }
+
+        this.deckOfCards = newDeck;
+
+    }
 }
