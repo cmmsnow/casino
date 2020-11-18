721eb77e67eeeb441f70905501b03c561d9efd97
Earl Atta-Fynn <Earl.Atta.Fynn@gmail.com>
2020-11-18 13:14:29 -0500

    Skeleton up and running

--- src/main/java/io/zipcoder/casino/Players/BlackJackPlayer.java
+++ src/main/java/io/zipcoder/casino/Players/BlackJackPlayer.java
@@ -1,12 +1,31 @@
 package io.zipcoder.casino.Players;
 
-public class BlackJackPlayer extends GamblingPlayer implements GamblingPlayerInterface{
+import io.zipcoder.casino.GameSupplies.Card;
 
-    public BlackJackPlayer(String name, Double wallet) {
-        super(name, wallet);
+import java.util.ArrayList;
+
+public class BlackJackPlayer implements GamblingPlayerInterface{
+    private String name;
+    private Player player;
+    private double wallet;
+    private ArrayList<Card> hand;
+
+    public BlackJackPlayer(Player player) {
+        this.player = player;
+        this.name = player.getName();
+        this.wallet = player.getWallet();
+        this.hand = new ArrayList<Card>();
+    }
+
+    public double getWallet() {
+        return 0;
     }
 
     public void setWallet() {
 
+    }
+
+    public double placeBet() {
+        return 0;
     }
 }
--- src/main/java/io/zipcoder/casino/Players/CrapsPlayer.java
+++ src/main/java/io/zipcoder/casino/Players/CrapsPlayer.java
@@ -1,12 +1,25 @@
 package io.zipcoder.casino.Players;
 
-public class CrapsPlayer extends GamblingPlayer implements GamblingPlayerInterface{
+public class CrapsPlayer  implements GamblingPlayerInterface{
+    private String name;
+    private Player player;
+    private double wallet;
 
-    public CrapsPlayer(String name, double wallet) {
-        super(name, wallet);
+    public CrapsPlayer(Player player) {
+        this.player = player;
+        this.name = player.getName();
+        this.wallet = player.getWallet();
+    }
+
+    public double getWallet() {
+        return 0;
     }
 
     public void setWallet() {
 
+    }
+
+    public double placeBet() {
+        return 0.00;
     }
 }
