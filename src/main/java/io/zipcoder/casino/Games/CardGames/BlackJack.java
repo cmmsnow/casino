6e3c9f6f2f399935624d41927bdad05f9367c2fb
christian <christian@zipcodes-MBP-51.lan>
2020-11-18 08:33:15 -0500

    added comments to BlackJack class

--- src/main/java/io/zipcoder/casino/Games/CardGames/BlackJack.java
+++ src/main/java/io/zipcoder/casino/Games/CardGames/BlackJack.java
@@ -1,4 +1,47 @@
 package io.zipcoder.casino.Games.CardGames;
 
-public class BlackJack extends CardGames{
+import io.zipcoder.casino.Games.GamblingGameInterface;
+import io.zipcoder.casino.Players.GamblingPlayerInterface;
+
+public class BlackJack extends CardGames implements GamblingGameInterface {
+
 }
+
+
+/*
+
+private Player blackjackPlayer;
+private Integer playerHandTotal;
+private Integer houseHandTotal;
+
+
+public BlackJack(Player blackjackPlayer) {
+    this.blackjackPlayer = blackjackPlayer)
+}
+
+public void clearAllHands(){
+}
+
+public void hitOrStay() {
+}
+
+public double bet() {
+}
+
+public double payout() {
+}
+
+public void play() {
+}
+
+public String getPlayerDealerResponse() { // hit or stay
+}
+
+public void winConditions() {
+}
+
+
+
+
+
+ */
