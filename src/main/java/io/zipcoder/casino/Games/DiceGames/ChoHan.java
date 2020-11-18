89c4e5f93b6f1110dbd919ab6d5d6fa7e8b1d8f3
will <will@zipcodes-MBP-8.fios-router.home>
2020-11-18 15:57:55 -0500

    began cho han

--- src/main/java/io/zipcoder/casino/Games/DiceGames/ChoHan.java
+++ src/main/java/io/zipcoder/casino/Games/DiceGames/ChoHan.java
@@ -1,4 +1,44 @@
 package io.zipcoder.casino.Games.DiceGames;
+import io.zipcoder.casino.utilities.Console;
+import io.zipcoder.casino.Players.ChoHanPlayer;
+import io.zipcoder.casino.utilities.Input;
+import io.zipcoder.casino.utilities.Output;
 
-public class ChoHan {
-}
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ChoHan extends DiceGames {
+    private ChoHanPlayer choHanPlayer;
+    private Output output = new Output();
+    private boolean gameRuns = true;
+
+
+    public ChoHan(ChoHanPlayer choHanPlayer){
+        this.choHanPlayer = choHanPlayer;
+    }
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void startScreen() {
+        output.printToScreen("\nWelcome to Cho Han " + choHanPlayer.getName() + "!");
+
+        while (gameRuns){
+            output.printToScreen("\nWould you like to play a game of Cho Han " + choHanPlayer.getName() + "?\n" +
+                    "1 - Yes, play game of Cho Han\n" +
+                    "2 - Quit Cho Han and return to Casino floor\n" +
+                    "----------------------------------------------\n");
+            int input = Input.getIntInput("Enter decision here -> ");
+
+            try {
+            } catch (InputMismatchException e) {
+                scanner.next();
+                output.printToScreen("\n" + "Sorry! Please try again and choose a valid option.");
+            }
+
+        }
+
+
+        }
+
+    }
+
--- src/main/java/io/zipcoder/casino/Games/DiceGames/DiceGames.java
+++ src/main/java/io/zipcoder/casino/Games/DiceGames/DiceGames.java
@@ -1,4 +1,6 @@
 package io.zipcoder.casino.Games.DiceGames;
 
+import io.zipcoder.casino.GameSupplies.Dice;
+
 public class DiceGames {
 }
